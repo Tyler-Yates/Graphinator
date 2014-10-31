99d931e7e99a72bc9a086ee6428664316136dada
Tyler Yates <tyler.yates@utexas.edu>
2014-10-31 12:16:30 -0500

    Add unit test for removing a vertex

--- src/test/java/graph/VertexManagerTest.java
+++ src/test/java/graph/VertexManagerTest.java
@@ -9,14 +9,17 @@ import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
+@SuppressWarnings("JavaDoc")
 public class VertexManagerTest {
 
     private VertexManager vertexManager;
+    private ConnectionManager connectionManager;
 
     @Before
     public void init() {
         Graph graph = new Graph();
-        vertexManager = new VertexManager(graph);
+        vertexManager = graph.getVertexManager();
+        connectionManager = graph.getConnectionManager();
     }
 
     @Test
@@ -55,5 +58,28 @@ public class VertexManagerTest {
         assertNotSame(vertex1, vertexManager.getVertex(vertexID1));
         // Assert that the same number of vertices exist
         assertEquals(2, vertexManager.numberOfVertices());
+    }
+
+    @Test
+    public void testRemoveVertex() {
+        // Create three vertices
+        final int id1 = vertexManager.createVertex(new CanvasPosition(0, 0));
+        final int id2 = vertexManager.createVertex(new CanvasPosition(1, 1));
+        final int id3 = vertexManager.createVertex(new CanvasPosition(2, 2));
+        assertEquals(3, vertexManager.numberOfVertices());
+        final Vertex vertex1 = vertexManager.getVertex(id1);
+        final Vertex vertex2 = vertexManager.getVertex(id2);
+        final Vertex vertex3 = vertexManager.getVertex(id3);
+
+        // Create two connections between the vertices
+        connectionManager.addConnection(vertex1, vertex2);
+        connectionManager.addConnection(vertex3, vertex1);
+        assertEquals(2, connectionManager.numConnections());
+
+        // Remove the vertex that is an end of both connections
+        vertexManager.removeVertex(vertex1);
+        // There should now be only two vertices and one connection
+        assertEquals(2, vertexManager.numberOfVertices());
+        assertEquals(0, connectionManager.numConnections());
     }
 }
