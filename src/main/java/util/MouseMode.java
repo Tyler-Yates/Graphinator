1607262710d8cf96c83b04345dc09d35270a236f
Tyler Yates <tyler.yates@utexas.edu>
2014-10-31 20:28:16 -0500

    Add enum documentation

--- src/main/java/util/MouseMode.java
+++ src/main/java/util/MouseMode.java
@@ -1,5 +1,8 @@
 package util;
 
+/**
+ * Represents the mode the mouse is in which determines what happens when the user left-clicks.
+ */
 public enum MouseMode {
-    VERTEX, CONNECTION, REMOVE, SAVE, LOAD
+    VERTEX, CONNECTION, REMOVE
 }
--- src/main/java/visualization/ButtonState.java
+++ src/main/java/visualization/ButtonState.java
@@ -1,5 +1,8 @@
 package visualization;
 
+/**
+ * Represents the state of the buttons with relations to selection and the mouse.
+ */
 public enum ButtonState {
     NORMAL, HOVER, SELECTED
 }
