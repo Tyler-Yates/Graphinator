4797d179d0d7c5a859b941b5290b2d3a388621b0
Tyler Yates <tyler.yates@utexas.edu>
2015-09-16 14:09:56 -0500

    Implement proper coloring that works under all circumstances. This is very large performance hit.

--- src/main/java/graph/ColorManager.java
+++ src/main/java/graph/ColorManager.java
@@ -1,17 +1,16 @@
 package graph;
 
+import com.google.common.collect.Collections2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.awt.*;
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Random;
 import java.util.Set;
 
@@ -88,89 +87,71 @@ class ColorManager {
      * Assigns colors to all of the vertices in the graph.
      */
     void assignColors() {
-        // Assume the graph is connected. This property will be checked in the process of
-        // coloring vertices.
-        graph.properties().graphIsConnected();
-        if (graph.getVertexManager().numberOfVertices() == 0) {
-            return;
-        }
+        final Collection<List<Vertex>> vertexPermutations = Collections2.orderedPermutations(
+                graph.getVertices());
 
-        LOGGER.debug("assigning colors-------------------------");
-
-        // Reset the maximum color as we are reassigning all colors
-        maximumColor = -1;
-        final Iterator<Vertex> vertexIterator = graph.getVertexManager().getVertices().iterator();
-        final Set<Vertex> verticesWithColorAssigned = new HashSet<>();
-
-        // Start the color assignment from a random vertex in the graph
-        processFromVertex(vertexIterator.next(), verticesWithColorAssigned);
-
-        // If there are still vertices we have not assigned colors to then the graph is unconnected
-        if (verticesWithColorAssigned.size() < graph.getVertexManager().numberOfVertices()) {
-            graph.properties().graphIsDisconnected();
-        }
-        while (verticesWithColorAssigned.size() < graph.getVertexManager().numberOfVertices()) {
-            final Vertex vertex = vertexIterator.next();
-            if (!verticesWithColorAssigned.contains(vertex)) {
-                processFromVertex(vertex, verticesWithColorAssigned);
+        maximumColor = 0;
+        int smallestColoring = Integer.MAX_VALUE;
+        ColorMap smallestColoringMap = null;
+        for (final List<Vertex> vertexOrdering : vertexPermutations) {
+            final ColorMap colorMap = getColorMap(vertexOrdering);
+            if (colorMap.getMaxColor() < smallestColoring) {
+                smallestColoring = colorMap.getMaxColor();
+                smallestColoringMap = colorMap;
             }
+        }
+
+        if (smallestColoringMap != null) {
+            smallestColoringMap.assignColors();
+            maximumColor = smallestColoringMap.getMaxColor();
         }
     }
 
-    private void processFromVertex(Vertex vertex, Set<Vertex> verticesWithColorAssigned) {
-        final Queue<Vertex> next = new LinkedList<>();
-        final Map<Vertex, Set<Integer>> forbiddenColors = new HashMap<>();
+    private ColorMap getColorMap(List<Vertex> vertexOrdering) {
+        final ColorMap colorMap = new ColorMap();
 
-        // Add the first vertex to next
-        next.add(vertex);
-
-        while (!next.isEmpty()) {
-            final Vertex current = next.poll();
-            LOGGER.debug("current vertex: " + current);
-
-            // Assign the first valid color to current
-            final int assignedColor = pickFirstValidColor(forbiddenColors.get(current));
-            if (assignedColor > maximumColor) {
-                maximumColor = assignedColor;
-            }
-            current.setColor(assignedColor);
-            LOGGER.debug("assigned color: " + assignedColor);
-
-            verticesWithColorAssigned.add(current);
-
-            final Set<Vertex> neighbors = current.getNeighbors();
-            LOGGER.debug("checking neighbors: " + neighbors);
-            for (Vertex neighbor : neighbors) {
-                // Look only at vertices that have not yet been assigned a color
-                if (!verticesWithColorAssigned.contains(neighbor)) {
-                    LOGGER.debug("checking unassigned neighbor: " + neighbor);
-                    final Set<Integer> forbidden = forbiddenColors.get(neighbor);
-                    if (forbidden == null) {
-                        final Set<Integer> newForbidden = new HashSet<>();
-                        newForbidden.add(assignedColor);
-                        forbiddenColors.put(neighbor, newForbidden);
-                    } else {
-                        forbidden.add(assignedColor);
-                    }
-                    LOGGER.debug("neighbor's forbidden colors: " + forbiddenColors.get(neighbor));
-
-                    next.add(neighbor);
-                } else {
-                    LOGGER.debug("skipping assigned neighbor: " + neighbor);
+        for (final Vertex vertex : vertexOrdering) {
+            final Set<Integer> neighborColors = new HashSet<>();
+            for (final Vertex neighbor : vertex.getNeighbors()) {
+                final Integer neighborColor = colorMap.getColor(neighbor);
+                if (neighborColor != null) {
+                    neighborColors.add(neighborColor);
                 }
             }
+
+            int color = 0;
+            while (neighborColors.contains(color)) {
+                color++;
+            }
+            colorMap.setColor(vertex, color);
         }
+
+        return colorMap;
     }
 
-    private int pickFirstValidColor(Set<Integer> forbiddenColors) {
-        if (forbiddenColors == null) {
-            return 0;
+    class ColorMap {
+        int maxColor = 0;
+        final Map<Vertex, Integer> colorMapping = new HashMap<>();
+
+        public Integer getColor(Vertex v) {
+            return colorMapping.get(v);
         }
 
-        int color = 0;
-        while (forbiddenColors.contains(color)) {
-            color++;
+        public void setColor(Vertex v, int color) {
+            colorMapping.put(v, color);
+            if (color > maxColor) {
+                maxColor = color;
+            }
         }
-        return color;
+
+        public void assignColors() {
+            for (final Map.Entry<Vertex, Integer> vertexAndColor : colorMapping.entrySet()) {
+                vertexAndColor.getKey().setColor(vertexAndColor.getValue());
+            }
+        }
+
+        public int getMaxColor() {
+            return maxColor;
+        }
     }
 }
